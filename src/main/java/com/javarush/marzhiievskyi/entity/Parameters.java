2eb9bd05d2de12b001e41be61b6d0fc35d27ad6a
Dmytro M <dmytromarzhiievskyi@gmail.com>
2022-10-11 08:23:04 +0300

    added param names using unicode img

--- src/main/java/com/javarush/marzhiievskyi/entity/Parameters.java
+++ src/main/java/com/javarush/marzhiievskyi/entity/Parameters.java
@@ -1,8 +1,27 @@
 package com.javarush.marzhiievskyi.entity;
 
-
-
 public class Parameters {
+
+    //Names & symbols of animals
+    //Carnivorous
+    public final static String NAME_WOOLF = "\uD83D\uDC3A";
+    public final static String NAME_SNAKE = "\uD83D\uDC0D";
+    public final static String NAME_FOX = "\uD83E\uDD8A";
+    public final static String NAME_BEAR = "\uD83D\uDC3B";
+    public final static String NAME_EAGLE = "\uD83E\uDD85";
+    //Herbivorous
+    public final static String NAME_HORSE = "\uD83D\uDC0E";
+    public final static String NAME_DEER = "\uD83E\uDD8C";
+    public final static String NAME_RABBIT = "\uD83D\uDC07";
+    public final static String NAME_MOUSE = "\uD83D\uDC01";
+    public final static String NAME_GOAT = "\uD83D\uDC10";
+    public final static String NAME_SHEEP = "\uD83D\uDC11";
+    public final static String NAME_BOAR = "\uD83D\uDC17";
+    public final static String NAME_BUFFALO = "\uD83D\uDC03";
+    public final static String NAME_DUCK = "\uD83E\uDD86";
+    public final static String NAME_CATERPILLAR = "\uD83D\uDC1B";
+
+
 
 
 
